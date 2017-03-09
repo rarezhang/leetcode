64c3dd3fd1d2a414d641ca64c040f50fd6d6de77
rarezhang <catezhang@qq.com>
2017-03-09 04:20:08 +0000

    hash table

---
+++ hash_table/290.WordPattern.py
@@ -0,0 +1,59 @@
+"""
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+"""
+
+'''
+# top solution 
+# pythonic 
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        s = pattern
+        t = str.split()
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
+'''
+        
+# top solution         
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = str.split()
+        if len(pattern) != len(words):
+            return False
+        dic_p = {}
+        dic_w = {}
+        for i in range(len(words)):
+            
+            if pattern[i] in dic_p:
+                if dic_p[pattern[i]] != words[i]:
+                    return False
+            if words[i] in dic_w:
+                if dic_w[words[i]] != pattern[i]:
+                    return False
+            dic_p[pattern[i]] = words[i]
+            dic_w[words[i]] = pattern[i]    
+        return True 
+        
+sol = Solution()
+pattern = "abba"
+str = "dog dog dog dog"
+r = sol.wordPattern(pattern, str)
+print(r)
